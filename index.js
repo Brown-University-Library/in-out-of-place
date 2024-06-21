436581827ffa46c1318812f80886e6a6c10007a7
E. Patrick Rashleigh <edward_rashleigh@brown.edu>
2024-06-21 12:21:39 -0400

    Move JS in index.html to new JS file

---
+++ index.js
@@ -0,0 +1,64 @@
+let overlay;
+
+function init() {
+  let mapBounds = new L.LatLngBounds(
+    new L.LatLng(-85.051129, -180),
+    new L.LatLng(85.051129, 180)
+  );
+  let mapMinZoom = 1;
+  let mapMaxZoom = 11;
+  let map = L.map("map", { minZoom: mapMinZoom, maxZoom: mapMaxZoom }).setView(
+    [43.869281, -103.5264874],
+    6
+  );
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+  overlay = L.tileLayer("https://cawm.lib.uiowa.edu/tiles/{z}/{x}/{y}.png", {
+    minZoom: mapMinZoom,
+    maxZoom: mapMaxZoom,
+    bounds: mapBounds,
+    opacity: 0.95,
+  }).addTo(map);
+
+  let sites = "geojson/sites.geojson";
+
+  fetch(sites)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("Here is the GeoJSON sites data object: ", L.geoJSON(data));
+      L.geoJSON(data).addTo(map);
+    })
+    .catch(function (error) {
+      console.log(`This is the error: ${error}`);
+    });
+
+  let expo = "geojson/expo.geojson";
+
+  fetch(expo)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("Here is the GeoJSON expo data object: ", L.geoJSON(data));
+      L.geoJSON(data).addTo(map);
+    })
+    .catch(function (error) {
+      console.log(`This is the error: ${error}`);
+    });
+
+  const changeMapFunction = function () {}; // placeholder
+
+  //need js code to connect geojson data with timeslider//
+
+  L.control
+    .timeslider({
+      timelineItems: [
+        "Long Ago",
+        "1851",
+        "1868",
+        "1874",
+        "1877",
+        "1889",
+        "Today",
+      ],
+      changeMap: changeMapFunction,
+    })
+    .addTo(mymap);
+}
